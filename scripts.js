a62e831fd350dabc971abb030e10122d42143ca4
danmschaffer <110274789+danmschaffer@users.noreply.github.com>
2022-08-11 22:37:46 -0400

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -40,20 +40,7 @@ button.addEventListener('click', () => {
         $('.menu-btn i').toggleClass("active");
     });
 
-    // typing text animation script
-    var typed = new Typed(".typing", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
 
-    var typed = new Typed(".typing-2", {
-        strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
 
     // owl carousel script
     $('.carousel').owlCarousel({
